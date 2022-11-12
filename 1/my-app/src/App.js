1b5d28987518c3d7e328f1c669f9d57e54db0da4
qwerty22166 <nurjusu184@gmail.com>
2022-11-13 01:21:24 +0600

    Add

---
+++ 1/my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AboutUs(props) {
+  return <h1>{props.text}</h1>
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AboutUs text="title"/>
+    </div>
+  );
+}
+
+export default App;
